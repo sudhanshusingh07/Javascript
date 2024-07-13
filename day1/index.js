127862de8217c47f8e068cdd794705c5c8fdd10b
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-13 10:11:45 +0530

    Update index.js

--- day1/index.js
+++ day1/index.js
@@ -1,29 +1,29 @@
-var a=1
-console.log(5)
+var a=1         //declare a variable useing var and assign a number
+console.log(a)  //log the value to the console
 
-let b="sudhanshu"
-console.log(b)
-
-
-const c=true
-console.log(c)
-
-const d=[1,2,3,4]
-console.log(d)
+let b="sudhanshu"  //declare a variable useing let and assign a string
+console.log(b)      //log the value to the console
 
 
-const e=new Object();
+const c=true        //declare a variable useing const and assign a boolean
+console.log(c)      //log the value to the console
+
+const d=[1,2,3,4]   //declare a variable useing const and assign a array
+console.log(d)      //log the value to the console
+
+
+const e=new Object();       //declare a variable useing const and assign a object
     e.name="sudhanshu";
     e.age=21;
-    console.log(e)
+    console.log(e)      //log the value to the console
 
-b=77
+b=77                    //assign new value in let
 console.log(b)
 
-c=8
-console.log(c)
+// c=8                      //try assign in const 
+// console.log(c)               it show error
 
-console.log(typeof(a))
+console.log(typeof(a))          //type of opreter
 console.log(typeof(b))
 console.log(typeof(c))
 console.log(typeof(d))
