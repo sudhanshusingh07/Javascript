314904840b06bf2710c48106af950e5a4d2b9b5d
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-13 09:56:22 +0530

    Initial commit

---
+++ day1/index.js
@@ -0,0 +1,30 @@
+var a=1
+console.log(5)
+
+let b="sudhanshu"
+console.log(b)
+
+
+const c=true
+console.log(c)
+
+const d=[1,2,3,4]
+console.log(d)
+
+
+const e=new Object();
+    e.name="sudhanshu";
+    e.age=21;
+    console.log(e)
+
+b=77
+console.log(b)
+
+c=8
+console.log(c)
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
