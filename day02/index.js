99a0cadcb60e8ec056f838b9d2ffc65915028682
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-14 20:44:49 +0530

    day 2

---
+++ day02/index.js
@@ -0,0 +1,64 @@
+let a=10;
+let b=5;
+
+const add=a+b;   //addition of two number
+console.log("add " + add);
+
+const s=a-b;   //subtract of two number
+console.log("subtract " + s);
+
+const m=a*b;   //multiply of two number
+console.log("multiply " + m);
+
+const d=a/b;   //divide of two number
+console.log("divide " + d);
+
+const r=a%b;   //remainder of two number
+console.log("remainder " + d);
+
+
+a+=30;          
+b-=3;
+console.log("a:"+a+"b:"+b);
+
+if(a>b)
+    console.log("hello");
+
+if(b<a)
+    console.log("how are you");
+
+if(a>=b)
+    console.log("hello");
+
+if(b<=a)
+    console.log("how are you");
+
+
+a=b;
+
+if(a==b)
+    console.log("my name is sudhanshu");
+
+if(a===b)
+    console.log("my name is sudhanshu");
+
+
+a=2
+b=5
+
+if(a<3&&b<5)
+    console.log("hi")
+
+
+if(a<3 || b<5)
+    console.log("hi")
+
+
+if(a!=0)
+    console.log("hello")
+
+
+let z= a>5 ? a : b;     //ternary operater it is like if else if condition(a>5 in this case) is true then it z=a else z=b
+
+console.log(z);
+
