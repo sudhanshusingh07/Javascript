fd652d6dec8efcd9e89ac08f516f36649fe1c0ef
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-27 17:45:05 +0530

    Create task2.js

---
+++ day07/task2.js
@@ -0,0 +1,11 @@
+//access and log title and author properties of book 
+
+
+const book={
+    title:"js book",
+    author:"Hitesh",
+    year:2024
+}
+
+console.log(book.title);
+console.log(book.author);
