c52a42d8ad65d34ebfbb8308ff9eedb1765700ad
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-27 17:43:00 +0530

    Create task1.js

---
+++ day07/task1.js
@@ -0,0 +1,10 @@
+//create an object represrnting a book with properties like title,author,year,and log the object to the console.
+
+
+const book={
+    title:"js book",
+    author:"Hitesh",
+    year:2024
+}
+
+console.log(book);
