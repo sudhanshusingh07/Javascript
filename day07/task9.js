94fb7bb496f6d37ee74daf37b42d6a89da2f629a
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-27 18:32:46 +0530

    Create task9.js

---
+++ day07/task9.js
@@ -0,0 +1,7 @@
+// Use Object.keys and Object.values methods to log all the keys and values of the book object.
+let book = {
+    title:"js",
+    author: "ram",
+    year: 2020,
+  };
+console.log(Object.keys(book), Object.values(book));
