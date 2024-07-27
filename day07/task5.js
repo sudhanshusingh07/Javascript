24f75516c8a08dfeb307a79f2bce575053a8fac3
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-07-27 18:12:41 +0530

    Create task5.js

---
+++ day07/task5.js
@@ -0,0 +1,28 @@
+//Create a nested object representing a library with properties like name and books (an array of book objects), and log the library object to the console.
+
+
+
+const library={
+    name: "School Library",
+    book:[
+        {
+    title:"js book",
+    author:"Hitesh",
+    year:2024},
+
+        {
+        title:"c++ book",
+        author:"dev",
+        year:2010},
+
+            {
+            title:"java book",
+            author:"deepak",
+            year:2020},
+
+]};
+
+
+
+console.log(library);
+
